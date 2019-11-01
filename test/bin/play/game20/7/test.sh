182e96e22e2b66cde01623d950bed155e13f9841
John McFarlane <git@john.mcfarlane.name>
2019-11-01 01:29:04 +0000

    New tests

---
+++ test/bin/play/game20/7/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# installs tools necessary to execute project scripts on Debian;
+# tested on Buster and Ubuntu 19.04
+
+set -e
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
+
+./src/play/play \
+  orlfedi \
+  "$1/words/wwf.txt" \
+  "${SCRIPT_DIR}/tiles.csv" \
+  "$1/boards/wwf_challenge.csv" 
