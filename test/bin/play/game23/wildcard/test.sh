7c5fbbfd7159dc7426e39605e4687cea76139811
John McFarlane <git@john.mcfarlane.name>
2019-11-02 23:11:07 +0000

    Add wildcard letter to input rack
    - same as blank but with a score
    - useful for calculating worst case scenario if you don't know competitors'
      letters

---
+++ test/bin/play/game23/wildcard/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# installs tools necessary to execute project scripts on Debian;
+# tested on Buster and Ubuntu 19.04
+
+set -e
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
+
+./src/play/play \
+  ??????? \
+"${SCRIPT_DIR}/tiles.csv" \
+  "$1/boards/wwf_challenge.csv" 
