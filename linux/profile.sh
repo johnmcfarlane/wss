ebcba75483cdba41e5917b7d993016712aa1ab6a
John McFarlane <git@john.mcfarlane.name>
2019-11-02 15:41:43 +0000

    Clean up profile shell script
    - add missing variable
    - no echo commands
    - dead code

--- linux/profile.sh
+++ linux/profile.sh
@@ -2,9 +2,10 @@
 
 # run in build directory to perform dynamic analysis
 
-set -ex
+set -e
 
 PROJECT_DIR=$(cd $(dirname "$0")/..; pwd)
+NUM_CPUS=$(nproc)
 
 "${PROJECT_DIR}/linux/config-cmake.sh" \
   -DCMAKE_CXX_FLAGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -g -DGSL_UNENFORCED_ON_CONTRACT_VIOLATION -fno-omit-frame-pointer"
@@ -13,4 +14,3 @@ cmake --build . -- --jobs ${NUM_CPUS}
 
 perf record -g ./src/play/play abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_ ../words/wwf.txt ../test/bin/play/game14/9/tiles.csv
 perf report -g 'graph,0.5,caller'
-#"${PROJECT_DIR}/linux/run-acceptance-tests.sh"
