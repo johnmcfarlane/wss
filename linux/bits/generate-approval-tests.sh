9b253600b828adaf4ae1fe7870cf0db9c9ef2821
John McFarlane <git@john.mcfarlane.name>
2020-01-18 01:19:03 +0000

    Parameterise approval test script on test directory
    - makes it easier to generate new acceptance test output without having to
      re-generate everything

--- linux/bits/generate-approval-tests.sh
+++ linux/bits/generate-approval-tests.sh
@@ -6,7 +6,7 @@ set -e
 
 PROJECT_DIR=$(cd $(dirname "$0")/../..; pwd)
 
-find "${PROJECT_DIR}/test/bin" -name "test.sh" | while read TEST; do
+find "$1" -name "test.sh" | while read TEST; do
   TEST_DIR="$(dirname $TEST)"
   "${TEST}" "${PROJECT_DIR}" > "${TEST_DIR}/expected.stdout" 
 done;
--- linux/generate-approval-tests.sh
+++ linux/generate-approval-tests.sh
@@ -8,6 +8,7 @@ PROJECT_DIR=$(cd $(dirname "$0")/..; pwd)
 
 "${PROJECT_DIR}/linux/build.sh"
 
-"${PROJECT_DIR}/linux/bits/generate-approval-tests.sh"
+"${PROJECT_DIR}/linux/bits/generate-approval-tests.sh" \
+  "${PROJECT_DIR}/test/bin"
 
 echo success
