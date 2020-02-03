d58b064de8c5a39649a2ade64ccd771b54507538
John McFarlane <git@john.mcfarlane.name>
2020-02-03 21:24:24 +0000

    Make it easier to initialize conan w. multiple compilers
    - allow user to fix conan-profile file after Conan has chosen the wrong
      compile
    - hacky but works

--- scripts/bits/init.sh
+++ scripts/bits/init.sh
@@ -8,7 +8,9 @@ PROJECT_DIR=$(cd "$(dirname "$0")"/../..; pwd)
 
 pip3 install --quiet conan
 conan remote add --force my-conan-repo https://api.bintray.com/conan/bincrafters/public-conan
-conan profile new ./conan-profile --detect --force
+if [[ ! -f ./conan-profile ]]; then
+  conan profile new ./conan-profile --detect
+fi
 conan install \
   --build=missing "${PROJECT_DIR}" \
   --profile=./conan-profile
