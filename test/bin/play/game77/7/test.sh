ac022cca67538b629ef920705317859eee1c0c13
John McFarlane <git@john.mcfarlane.name>
2020-02-05 20:47:07 +0000

    Games 75+

---
+++ test/bin/play/game77/7/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+PROJECT_DIR=$1
+shift
+
+"$@" ./src/play/play \
+  JATVIEN \
+"${SCRIPT_DIR}/tiles.csv" \
+  "${PROJECT_DIR}/boards/wwf_challenge.csv" 
