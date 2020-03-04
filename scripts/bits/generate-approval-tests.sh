fce060ede0f2e79ebdba0816bedb4f73432db40e
John McFarlane <git@john.mcfarlane.name>
2020-03-04 12:48:51 +0000

    Make test status messages more consistent
    - Anything using the invoke-approval-tests.sh script now relies on it to
      state success or failure.

--- scripts/bits/generate-approval-tests.sh
+++ scripts/bits/generate-approval-tests.sh
@@ -7,15 +7,5 @@ set -eo pipefail
 BITS_DIR=$(cd "$(dirname "$0")"; pwd)
 TESTS_PATH=$1
 
-set +e
-
 "${BITS_DIR}/invoke-approval-tests.sh" \
   "${BITS_DIR}/generate-approval-test.sh" "${TESTS_PATH}"
-
-if [[ $? -ne 0 ]] ; then
-  echo failure
-  exit 1
-else
-  echo success
-  exit 0
-fi
