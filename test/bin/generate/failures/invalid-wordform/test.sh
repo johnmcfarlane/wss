cbe02cc0003160cac024f776913925b93dea0341
John McFarlane <git@john.mcfarlane.name>
2020-03-06 19:30:08 +0000

    Filter invalid words while generating lexicon

---
+++ test/bin/generate/failures/invalid-wordform/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BUILD_DIR=$(pwd)
+SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
+shift
+
+cd "${SCRIPT_DIR}" || exit
+
+"$@" "${BUILD_DIR}/src/generate/generate" \
+  lexicon.txt lexicon \
+  lexicon.txt lexicon \
+  lexicon
+
+if [[ $? -ne 1 ]] ; then
+    exit 1
+fi
