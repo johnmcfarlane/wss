5ae19cf735afbdaf963b67d0f2df600196085560
John McFarlane <git@john.mcfarlane.name>
2020-03-20 18:58:23 +0000

    Games 206 and 210

---
+++ test/bin/play/game210/7/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+PROJECT_DIR=$1
+shift
+
+"$@" ./src/play/play \
+  qijdent \
+  "${SCRIPT_DIR}/tiles.txt" \
+  "${PROJECT_DIR}/boards/wwf_challenge.txt"
