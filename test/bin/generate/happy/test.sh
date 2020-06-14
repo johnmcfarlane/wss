63fe13431006acf22acf1a149fd8c2789e088bdc
John McFarlane <git@john.mcfarlane.name>
2020-06-14 01:12:42 +0100

    Add a happy path test for generate program

---
+++ test/bin/generate/happy/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+shift
+
+"$@" ./src/generate/generate \
+  "${SCRIPT_DIR}"/lexicon.txt lexicon \
+  "${SCRIPT_DIR}"/lexicon.txt lexicon \
+  lexicon
+
+cat lexicon.cpp lexicon.h
