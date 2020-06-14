b7ef2791437613cce5ded8922f1ef52648502a27
John McFarlane <git@john.mcfarlane.name>
2020-06-14 12:43:29 +0100

    Generate approval tests with Release build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ PROJECT_DIR=$(cd "$(dirname "$0")"/..; pwd)
 
 "${PROJECT_DIR}/scripts/bits/init.sh"
 
-"${PROJECT_DIR}/scripts/bits/config.sh"
+"${PROJECT_DIR}/scripts/bits/config.sh" \
+  "$@"
 
 "${PROJECT_DIR}/scripts/bits/build.sh"
--- scripts/generate-approval-tests.sh
+++ scripts/generate-approval-tests.sh
@@ -6,7 +6,8 @@ set -eo pipefail
 
 PROJECT_DIR=$(cd "$(dirname "$0")"/..; pwd)
 
-"${PROJECT_DIR}/scripts/build.sh"
+"${PROJECT_DIR}/scripts/build.sh" \
+  -DCMAKE_BUILD_TYPE=Release
 
 "${PROJECT_DIR}/scripts/bits/generate-approval-tests.sh" \
   "${PROJECT_DIR}/test/bin"
