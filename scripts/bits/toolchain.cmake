4aef3cd2b7f3bf8247a864dd10b892054e11b68e
John McFarlane <git@john.mcfarlane.name>
2020-06-14 12:43:01 +0100

    ditch WSS_LEXICON_COMPILE_OPTIONS
    - compile times don't appear to be an issue with
      release builds in Clang or GCC 10
    - binaries were bloated as a result:
      - before:
        - liblexicon: 46346346
        - play: 46778944
      - after:
        - liblexicon: 26883322
        - play: 27318808

--- scripts/bits/toolchain.cmake
+++ scripts/bits/toolchain.cmake
@@ -1,6 +1,1 @@
 SET(CMAKE_CXX_FLAGS "-Werror -Wall -Wextra -Wpedantic")
-
-# scrabble_lexicon.cpp and wwf_lexicon.cpp are enormous data definition sources.
-# They take an exceedingly long time to compile under GCC if the optimizer is
-# enabled.
-SET(WSS_LEXICON_COMPILE_OPTIONS "-O0")
