610a2faf617f7cf459077cb3399ec7c1b7c96fb6
John McFarlane <github@john.mcfarlane.name>
2021-09-20 22:57:36 +0100

    Replaced game5

--- test/bin/play/game5/3/test.sh
+++ test/bin/play/game5/3/test.sh
@@ -7,6 +7,6 @@ PROJECT_DIR=$1
 shift
 
 "$@" ./src/play/play \
-  opwiqeo \
+  lqeoxse \
   "${SCRIPT_DIR}/tiles.txt" \
   "${PROJECT_DIR}/boards/wwf_challenge.txt" 
--- test/bin/play/game5/5/test.sh
+++ test/bin/play/game5/5/test.sh
@@ -7,6 +7,6 @@ PROJECT_DIR=$1
 shift
 
 "$@" ./src/play/play \
-  oiqeo_z \
+  qoytter \
   "${SCRIPT_DIR}/tiles.txt" \
   "${PROJECT_DIR}/boards/wwf_challenge.txt" 
