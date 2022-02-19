d98524a08a7c54a4755714ec446fddf3e1e95279
John McFarlane <git@john.mcfarlane.name>
2022-02-19 04:03:30 +0000

    Reenable cert-err33-c

--- src/play/load_buffer.cpp
+++ src/play/load_buffer.cpp
@@ -32,15 +32,27 @@ auto load_buffer(gsl::cstring_span<> filename)
         return std::nullopt;
     }
 
-    std::fseek(f.get(), 0, SEEK_END);
+    // LCOV_EXCL_START - this doesn't even fail for directory; any suggestions on how to test gratefully received
+    if (std::fseek(f.get(), 0, SEEK_END) != 0) {
+        fmt::print(stderr, "error: Could not seek to the end of '{}'.\n", gsl::to_string(filename));
+        return std::nullopt;
+    }
+    // LCOV_EXCL_STOP
+
     auto const file_size{std::ftell(f.get())};
-    std::fseek(f.get(), 0, SEEK_SET);
+
+    // LCOV_EXCL_START
+    if (std::fseek(f.get(), 0, SEEK_SET) != 0) {
+        fmt::print(stderr, "error: Could not seek to the start of '{}'.\n", gsl::to_string(filename));
+        return std::nullopt;
+    }
+    // LCOV_EXCL_STOP
 
     std::vector<char> buffer(file_size);
     auto const read{static_cast<std::ptrdiff_t>(
             std::fread(buffer.data(), 1, file_size, f.get()))};
 
-    // LCOV_EXCL_START - can only happen if file changes size mid-test
+    // LCOV_EXCL_START - can only happen if file changes size mid-test?
     if (read != file_size) {
         fmt::print(stderr, "error: could not read, '{}'.\n", gsl::to_string(filename));
         return std::nullopt;
