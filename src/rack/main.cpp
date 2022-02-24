6948442076a12e4fa944a29e85a081c931d6c27d
John McFarlane <git@john.mcfarlane.name>
2022-02-24 22:52:51 +0000

    Clean up CMake target dependencies
    - remove unused lexicons
    - make target dependencies PRIVATE

--- src/rack/main.cpp
+++ src/rack/main.cpp
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <all_lexicon.h>
 #include <scores.h>
-#include <scrabble_lexicon.h>
 #include <ssize.h>
 #include <tile.h>
 #include <wwf_lexicon.h>
