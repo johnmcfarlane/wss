7343f9d19a6bf6907e4fc54c500b31a3d003e42c
John McFarlane <git@john.mcfarlane.name>
2022-02-26 13:47:55 +0000

    Replace happy path generator test with famous example

--- test/bin/generate/happy/expected_source.cpp
+++ test/bin/generate/happy/expected_source.cpp
@@ -1,90 +1,41 @@
 #include "expected_source.h"
 namespace {
-node const nLESS = {
+node const nCARS = {
   from_bits(0x1U),
   nullptr
 };
-node const nLESe[] {nLESS};
-node const nLES = {
-  from_bits(0x80000U),
-  nLESe
+node const nCARe[] {nCARS};
+node const nCAR = {
+  from_bits(0x80001U),
+  nCARe
 };
-node const nLEe[] {nLES};
-node const nLE = {
-  from_bits(0x80000U),
-  nLEe
+node const nCAe[] {nCAR,nCAR};
+node const nCA = {
+  from_bits(0x140000U),
+  nCAe
 };
-node const nLe[] {nLE};
-node const nL = {
+node const nCe[] {nCA};
+node const nC = {
+  from_bits(0x2U),
+  nCe
+};
+node const nDON = {
   from_bits(0x20U),
-  nLe
+  nCARe
 };
-node const nSPA = {
-  from_bits(0x100000U),
-  nLESe
+node const nDOe[] {nCAR,nDON};
+node const nDO = {
+  from_bits(0x4081U),
+  nDOe
 };
-node const nSPOTLESSNESS = {
-  from_bits(0x20U),
-  nLEe
+node const nDe[] {nDO};
+node const nD = {
+  from_bits(0x8000U),
+  nDe
 };
-node const nSPOTLESSNESe[] {nSPOTLESSNESS};
-node const nSPOTLESSNES = {
-  from_bits(0x80000U),
-  nSPOTLESSNESe
-};
-node const nSPOTLESSNEe[] {nSPOTLESSNES};
-node const nSPOTLESSNE = {
-  from_bits(0x80000U),
-  nSPOTLESSNEe
-};
-node const nSPOTLESSNe[] {nSPOTLESSNE};
-node const nSPOTLESSN = {
-  from_bits(0x20U),
-  nSPOTLESSNe
-};
-node const nSPOTLESSe[] {nSPOTLESSN};
-node const nSPOTLESS = {
-  from_bits(0x4000U),
-  nSPOTLESSe
-};
-node const nSPOTLESe[] {nSPOTLESS};
-node const nSPOTLES = {
-  from_bits(0x80000U),
-  nSPOTLESe
-};
-node const nSPOTLEe[] {nSPOTLES};
-node const nSPOTLE = {
-  from_bits(0x80000U),
-  nSPOTLEe
-};
-node const nSPOTLe[] {nSPOTLE};
-node const nSPOTL = {
-  from_bits(0x20U),
-  nSPOTLe
-};
-node const nSPOTe[] {nSPOTL};
-node const nSPOT = {
-  from_bits(0x1000U),
-  nSPOTe
-};
-node const nSPOe[] {nSPOT};
-node const nSPO = {
-  from_bits(0x100000U),
-  nSPOe
-};
-node const nSPe[] {nSPA,nSPO};
-node const nSP = {
-  from_bits(0x8002U),
-  nSPe
-};
-node const nSe[] {nSP};
-node const nS = {
-  from_bits(0x10000U),
-  nSe
-};
-node const root_nodee[] {nL,nS};
+node const root_nodee[] {nC,nD,nC};
 node const root_node = {
-  from_bits(0x81001U),
+  from_bits(0x39U),
   root_nodee
 };
 } //namespace
