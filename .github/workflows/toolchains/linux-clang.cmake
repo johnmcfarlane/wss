2c5aa50f77d96b8c36b42c181514b25f2f0b99aa
John McFarlane <git@john.mcfarlane.name>
2022-03-16 17:32:00 +0000

    Add GCC/Clang flags for trapping bugs
    - in standard library implementations
    - as recommended in
      https://github.com/johnmcfarlane/papers/blob/main/cpp/contractual-disappointment.md#appendix-a---toolchain-specific-recommendations

--- .github/workflows/toolchains/linux-clang.cmake
+++ .github/workflows/toolchains/linux-clang.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_CXX_FLAGS_COVERAGE_INIT
     "-coverage -fno-exceptions -DWSS_DISABLE_ASSERTS")
 set(CMAKE_CXX_FLAGS_TEST_INIT
-    "-DNDEBUG -O3 -Wall -Werror -Wextra -Wpedantic -fsanitize=address,undefined"
-)
+    "-D_LIBCPP_DEBUG=1 -DNDEBUG -O3 -Wall -Werror -Wextra -Wpedantic \
+-fsanitize=address,undefined")
--- .github/workflows/toolchains/linux-gcc.cmake
+++ .github/workflows/toolchains/linux-gcc.cmake
@@ -1,5 +1,6 @@
 set(CMAKE_CXX_FLAGS_COVERAGE_INIT
     "-coverage -fno-exceptions -DWSS_DISABLE_ASSERTS")
 set(CMAKE_CXX_FLAGS_TEST_INIT
-    "-DNDEBUG -O3 -Wall -Werror -Wextra -Wno-maybe-uninitialized \
--Wno-restrict -Wpedantic -fsanitize=address,undefined")
+    "-D_GLIBCXX_ASSERTIONS -DNDEBUG -O3 -Wall -Werror \
+-Wextra -Wno-maybe-uninitialized -Wno-restrict -Wpedantic \
+-fsanitize=address,undefined")
