e6638ab8c9555101c48ff7324745e3e3910f2db6
John McFarlane <git@john.mcfarlane.name>
2022-03-26 08:26:13 +0000

    Add script for MSVC release build

---
+++ .github/workflows/conan/release-msvc.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/env pwsh
+
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+$PROJECT_DIR="$PSScriptRoot\..\..\.."
+
+conan install `
+  --build=missing `
+  --env CONAN_CMAKE_TOOLCHAIN_FILE="${PROJECT_DIR}\.github\workflows\toolchains\windows-msvc.cmake" `
+  --settings build_type=Release `
+  "${PROJECT_DIR}" `
+  "$@"
+
+conan build `
+  "${PROJECT_DIR}"
