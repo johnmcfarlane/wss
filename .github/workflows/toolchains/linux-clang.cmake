2c6b6fdc2cf811f4d95e43e028573b51f304c677
John McFarlane <git@john.mcfarlane.name>
2022-05-09 08:37:55 +0100

    Enforce dangling ) for CMake functions

--- .github/workflows/toolchains/linux-clang.cmake
+++ .github/workflows/toolchains/linux-clang.cmake
@@ -1,5 +1,7 @@
 set(CMAKE_CXX_FLAGS_INIT "-Wall -Werror -Wextra -pedantic")
 set(CMAKE_CXX_FLAGS_COVERAGE_INIT
-    "-coverage -fno-exceptions -DWSS_DISABLE_ASSERTS")
+    "-coverage -fno-exceptions -DWSS_DISABLE_ASSERTS"
+)
 set(CMAKE_CXX_FLAGS_TEST_INIT
-    "-D_LIBCPP_DEBUG=1 -DNDEBUG -O3 -fsanitize=address,undefined")
+    "-D_LIBCPP_DEBUG=1 -DNDEBUG -O3 -fsanitize=address,undefined"
+)
--- .github/workflows/toolchains/linux-libfuzzer.cmake
+++ .github/workflows/toolchains/linux-libfuzzer.cmake
@@ -2,9 +2,11 @@ include(ProcessorCount)
 ProcessorCount(WSS_NUM_PROCESSORS)
 
 set(CMAKE_CXX_FLAGS_INIT
-    "-Wall -Werror -Wextra -fsanitize=address,undefined -g -pedantic")
+    "-Wall -Werror -Wextra -fsanitize=address,undefined -g -pedantic"
+)
 set(WSS_FUZZER_FLAGS "-fsanitize=address,fuzzer,undefined;-DWSS_USE_LIBFUZZER")
 set(WSS_FUZZER_LINKER_FLAGS "-fsanitize=fuzzer")
 set(WSS_FUZZER_TEST_PARAMETERS
     -jobs=${WSS_NUM_PROCESSORS} -workers=${WSS_NUM_PROCESSORS}
-    -max_total_time=60)
+    -max_total_time=60
+)
