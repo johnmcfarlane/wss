6c26a53fa7644b8187ee41252f31c9072e0e4aaa
John McFarlane <git@john.mcfarlane.name>
2022-05-15 13:53:15 +0100

    Give the WSS package a name

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@ from conans import ConanFile, CMake
 
 
 class WssConan(ConanFile):
+    name = "wss"
     settings = "os", "compiler", "build_type", "arch"
     requires = "catch2/[2.x]", "fmt/[8.x]", "ms-gsl/[3.x]", "lyra/[1.5.x]"
     generators = "cmake_find_package"
