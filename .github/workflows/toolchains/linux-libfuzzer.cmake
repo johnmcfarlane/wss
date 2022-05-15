e1ee09dc3b9580041989910c9b110d1c9430485e
John McFarlane <git@john.mcfarlane.name>
2022-05-16 00:10:14 +0100

    Don't force libfuzzer to use all CPUs
    - possible OOM crashes as a result of flooring system

--- .github/workflows/toolchains/linux-libfuzzer.cmake
+++ .github/workflows/toolchains/linux-libfuzzer.cmake
@@ -3,7 +3,4 @@ ProcessorCount(WSS_NUM_PROCESSORS)
 
 set(WSS_FUZZER_FLAGS "-fsanitize=address,fuzzer,undefined;-DWSS_USE_LIBFUZZER")
 set(WSS_FUZZER_LINKER_FLAGS "-fsanitize=fuzzer")
-set(WSS_FUZZER_TEST_PARAMETERS
-    -jobs=${WSS_NUM_PROCESSORS} -workers=${WSS_NUM_PROCESSORS}
-    -max_total_time=60
-)
+set(WSS_FUZZER_TEST_PARAMETERS -jobs=${WSS_NUM_PROCESSORS} -max_total_time=30)
